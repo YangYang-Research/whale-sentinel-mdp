0a45237b4a9168777a4b7865d7ca9be3252fa1ce
noobpk <ltp.noobpk@gmail.com>
2025-06-03 22:49:44 +0700

    Refactor getProfile and syncProfile methods to use Validator for request validation; enhance response structure and error handling

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Models\WSService;
 use App\Models\WSAgent;
 use App\Traits\ProfileValidationTrait;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class ProfileController extends Controller
 {
@@ -14,20 +16,37 @@ class ProfileController extends Controller
     public function getProfile(Request $request)
     {
         try {
-            $validated = $request->validate([
-                'type' => 'required|in:agent,service',
-                'key'  => ['required','string', 'max:255']]);
-            
-            if ($validated['type'] === 'agent' && !preg_match('/^ws_agent_.*/', $validated['key'])) {
+            $validator = Validator::make($request->all(),[
+                'event_info' => 'required|string',
+                'payload.data.type' => ['required', Rule::in(['agent', 'service'])],
+                'payload.data.key'  => 'required|string|max:255',
+                'request_created_at' => [
+                    'required',
+                    'date_format:Y-m-d\TH:i:s\Z',
+                ],
+            ]); 
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'code' => 400,
+                    'status' => 'Fail',
+                    'message' => 'Validate Failed',
+                    'errors' => $validator->errors(),
+                ]);
+            }
+
+            if ($request['payload']['data']['type'] === 'agent' && !preg_match('/^ws_agent_.*/', $request['payload']['data']['key'])) {
                 return response()->json([
                     'status' => 'error',
                     'message' => 'The key must start with "ws_agent_" for type "agent".'
                 ], 422);
             }
 
-            $type = $validated['type'];
-            $key = $validated['key'];
-
+            $eventInfo = $request['event_info'];
+            $type = $request['payload']['data']['type'];
+            $key = $request['payload']['data']['key'];
+            $requestCreatedAt = $request['request_created_at'];
+            
             if ($type === 'agent') {
                 $agent = WsAgent::where('name', $key)->first();
                 if (!$agent) {
@@ -36,13 +55,18 @@ class ProfileController extends Controller
                         'message' => 'Agent not found'
                     ], 404);
                 }
+
                 return response()->json([
                     'status'  => 'success',
-                    'type'    => 'agent',
-                    'name'    => $agent->name,
+                    'message' => 'Request processed successfully',
                     'data'  => [
-                        'profile'     => $agent->profile,
+                        'type'    => 'agent',
+                        'name'    => $agent->name,
+                        'profile' => $agent->profile,
                     ],
+                    'event_info' => '',
+                    'request_created_at' => $requestCreatedAt,
+                    'request_processed_at' => now()->toIso8601String(),
                 ]);
             }
 
@@ -54,18 +78,24 @@ class ProfileController extends Controller
                         'message' => 'Service not found'
                     ], 404);
                 }
+
                 return response()->json([
                     'status'  => 'success',
-                    'type'    => 'service',
-                    'name'    => $service->name,
+                    'message' => 'Request processed successfully',
                     'data'  => [
-                        'profile'     => $service->profile,
+                        'type'    => 'service',
+                        'name'    => $service->name,
+                        'profile' => $service->profile,
                     ],
+                    'event_info' => '',
+                    'request_created_at' => $requestCreatedAt,
+                    'request_processed_at' => now()->toIso8601String(),
                 ]);
             }
 
             return response()->json(['message' => 'Invalid type'], 400);
         } catch (\Exception $e) {
+            dd($e);
             return response()->json([
                 'status' => 'error',
                 'message' => 'An error occurred while processing your request.',
@@ -76,22 +106,38 @@ class ProfileController extends Controller
     public function syncProfile(Request $request)
     {
         try {
-            $validated = $request->validate([
-                    'type' => 'required|in:agent,service',
-                    'key'  => ['required','string', 'max:255'],
-                    'profile' => 'required|array'
-                ]);
+            $validator = Validator::make($request->all(),[
+                'event_info' => 'required|string',
+                'payload.data.type' => ['required', Rule::in(['agent', 'service'])],
+                'payload.data.key'  => 'required|string|max:255',
+                'payload.data.profile' => 'required|array',
+                'request_created_at' => [
+                    'required',
+                    'date_format:Y-m-d\TH:i:s\Z',
+                ],
+            ]); 
 
-            if ($validated['type'] === 'agent' && !preg_match('/^ws_agent_.*/', $validated['key'])) {
+            if ($validator->fails()) {
+                return response()->json([
+                    'code' => 400,
+                    'status' => 'Fail',
+                    'message' => 'Validate Failed',
+                    'errors' => $validator->errors(),
+                ]);
+            }
+
+            if ($request['payload']['data']['type'] === 'agent' && !preg_match('/^ws_agent_.*/', $request['payload']['data']['key'])) {
                 return response()->json([
                     'status' => 'error',
                     'message' => 'The key must start with "ws_agent_" for type "agent".'
                 ], 422);
             }
 
-            $type = $validated['type'];
-            $key = $validated['key'];
-            $newProfileData = $validated['profile'];
+            $eventInfo = $request['event_info'];
+            $type = $request['payload']['data']['type'];
+            $key = $request['payload']['data']['key'];
+            $requestCreatedAt = $request['request_created_at'];
+            $newProfileData = $request['payload']['data']['profile'];
 
             $validation = $this->validatePayloadKeysExistHardcoded($newProfileData);
             if ($validation !== true) {
@@ -109,6 +155,7 @@ class ProfileController extends Controller
                         'message' => 'Agent not found'
                     ], 404);
                 }
+
                 $currentProfile = json_decode($agent->profile, true);
                 if (!is_array($currentProfile)) {
                     $currentProfile = ['profile' => []];
@@ -128,11 +175,15 @@ class ProfileController extends Controller
 
                 return response()->json([
                     'status'  => 'success',
-                    'type'    => 'agent',
-                    'name'    => $agent->name,
+                    'message' => 'Request processed successfully',
                     'data'  => [
-                        'profile'     => $agent->profile,
+                        'type'    => 'agent',
+                        'name'    => $agent->name,
+                        'profile' => $agent->profile,
                     ],
+                    'event_info' => '',
+                    'request_created_at' => $requestCreatedAt,
+                    'request_processed_at' => now()->toIso8601String(),
                 ]);
             }
 
@@ -159,16 +210,20 @@ class ProfileController extends Controller
                 
                 $currentProfile['profile'] = $this->deepMerge($currentProfile['profile'], $newProfileData);
 
-                $agent->profile = json_encode($currentProfile, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
-                $agent->save();
+                $service->profile = json_encode($currentProfile, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+                $service->save();
 
                 return response()->json([
                     'status'  => 'success',
-                    'type'    => 'service',
-                    'name'    => $service->name,
+                    'message' => 'Request processed successfully',
                     'data'  => [
-                        'profile'     => $service->profile,
+                        'type'    => 'service',
+                        'name'    => $service->name,
+                        'profile' => $service->profile,
                     ],
+                    'event_info' => '',
+                    'request_created_at' => $requestCreatedAt,
+                    'request_processed_at' => now()->toIso8601String(),
                 ]);
             }
 
